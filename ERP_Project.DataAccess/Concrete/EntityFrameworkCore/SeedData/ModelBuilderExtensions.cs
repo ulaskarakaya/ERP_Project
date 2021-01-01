f7d00f9b9ff3ec0f6a5576c11a9e81c048c121fa
ulaş karakaya <ulas_gs_11@hotmail.com>
2021-01-01 19:29:13 +0300

    Migration işlemleri yapıldı.

--- ERP_Project.DataAccess/Concrete/EntityFrameworkCore/SeedData/ModelBuilderExtensions.cs
+++ ERP_Project.DataAccess/Concrete/EntityFrameworkCore/SeedData/ModelBuilderExtensions.cs
@@ -12,9 +12,9 @@ namespace ERP_Project.DataAccess.Concrete.EntityFrameworkCore.SeedData
         {
             modelBuilder.Entity<CalisanKategori>().HasData(
                 new CalisanKategori { Id = 1, Adi = "İnsan Kaynakları" },
-                new CalisanKategori { Id = 1, Adi = "Üretim" },
-                new CalisanKategori { Id = 1, Adi = "Tedarik Zinciri" },
-                new CalisanKategori { Id = 1, Adi = "Müşteri İlişkileri" }
+                new CalisanKategori { Id = 2, Adi = "Üretim" },
+                new CalisanKategori { Id = 3, Adi = "Tedarik Zinciri" },
+                new CalisanKategori { Id = 4, Adi = "Müşteri İlişkileri" }
                 );
         }
     }
---
+++ ERP_Project.DataAccess/Migrations/20210101162738_InitialCreate.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERP_Project.DataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalisanKategorileri",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalisanKategorileri", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Calisanlar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Soyadi = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Telefon = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: true),
+                    Sifre = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Fotograf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AktifMi = table.Column<bool>(type: "bit", nullable: false),
+                    CalisanKategoriId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calisanlar", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Calisanlar_CalisanKategorileri_CalisanKategoriId",
+                        column: x => x.CalisanKategoriId,
+                        principalTable: "CalisanKategorileri",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CalisanKategorileri",
+                columns: new[] { "Id", "Adi" },
+                values: new object[,]
+                {
+                    { 1, "İnsan Kaynakları" },
+                    { 2, "Üretim" },
+                    { 3, "Tedarik Zinciri" },
+                    { 4, "Müşteri İlişkileri" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calisanlar_CalisanKategoriId",
+                table: "Calisanlar",
+                column: "CalisanKategoriId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calisanlar");
+
+            migrationBuilder.DropTable(
+                name: "CalisanKategorileri");
+        }
+    }
+}
